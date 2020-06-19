a8167eacb1b10b9fb7af9db960f6eccaeda5ab52
Jorian <Jorian@mail.ru>
2020-06-19 20:26:48 +0300

    Revert "first homework"
    This reverts commit 0a40d8b0

--- homeworks/lesson1/ex3.py
+++
@@ -1,6 +0,0 @@
-user_num = input("Введите число: ")
-while not user_num.isdigit():
-    user_num = input("Это не число.\nВведите число: ")
-user_num = int(user_num)
-user_result = user_num + user_num * 11 + user_num * 111
-print(user_result)

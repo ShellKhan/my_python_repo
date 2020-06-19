c9c4bf8e58d5360794957ad70ac563495965a926
Jorian <Jorian@mail.ru>
2020-06-19 20:34:41 +0300

    Git flow repair

--- homeworks/lesson2/ex1.py
+++
@@ -1,8 +0,0 @@
-# создаем список с элементами разных типов
-
-my_list = [1, 5, True, False, 20.34, 'qwerty', 'ogogo', b'efef', (2, 3, 4), ['a', 'b', 'c'], None]
-
-# проверяем тип элементов
-
-for el in my_list:
-    print(type(el))
--- homeworks/lesson2/ex4.py
+++
@@ -1,4 +0,0 @@
-user_var = input('Введите фразу: ')
-user_words = user_var.split(' ')
-for ind, word in enumerate(user_words, 1):
-    print(ind, word[:10])
